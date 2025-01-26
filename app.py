477d2832dac63e6c66877d6c657f509e09e0ba94
naoko-asano <asano@wyrd.co.jp>
2025-01-26 21:09:01 +0900

    chore: first commit

---
+++ app.py
@@ -0,0 +1,41 @@
+from flask import Flask, Response, render_template
+import cv2
+
+app = Flask(__name__)
+
+FPS = 30
+WIDTH = 640
+HEIGHT = 480
+
+def init_camera():
+    cap = cv2.VideoCapture('/dev/video0')
+    cap.set(cv2.CAP_PROP_FPS, FPS)
+    cap.set(cv2.CAP_PROP_FRAME_WIDTH, WIDTH)
+    cap.set(cv2.CAP_PROP_FRAME_HEIGHT, HEIGHT)
+    return cap
+
+def generate_frames():
+    cap = init_camera()
+    try:
+        while True:
+            success, frame = cap.read()
+            if not success:
+                yield b'--frame\r\nContent-Type: text/plain\r\n\r\nError capturing frame.\r\n'
+                break
+            else:
+                ret, buffer = cv2.imencode('.jpg', frame)
+                yield (b'--frame\r\n'
+                       b'Content-Type: image/jpeg\r\n\r\n' + buffer.tobytes() + b'\r\n')
+    finally:
+        cap.release()
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/video_feed')
+def video_feed():
+    return Response(generate_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
