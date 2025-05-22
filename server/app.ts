6c214df71b6332982cc93e4615dc69521fb8796a
naoko-asano <asano@wyrd.co.jp>
2025-05-22 23:20:13 +0900

    chore: 本番環境の起動コマンド変更

--- server/app.ts
+++ server/app.ts
@@ -21,7 +21,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "server", "public")));
+app.use(express.static(path.join(__dirname, "dist", "client")));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
