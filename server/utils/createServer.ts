c09782618f61973a08b583e7d7805643fa53e466
naoko-asano <asano@wyrd.co.jp>
2025-06-21 17:26:18 +0900

    feat: 開発環境でhttpでも起動できるようにした

---
+++ server/utils/createServer.ts
@@ -0,0 +1,20 @@
+import path from "path";
+import fs from "fs";
+import http from "http";
+import https from "https";
+import type { Express } from "express";
+
+const keyPath = path.resolve("server.key");
+const certPath = path.resolve("server.crt");
+
+export function createServer(app: Express): http.Server | https.Server {
+  if (fs.existsSync(keyPath) && fs.existsSync(certPath)) {
+    const options = {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.crt"),
+    };
+    return https.createServer(options, app);
+  } else {
+    return http.createServer(app);
+  }
+}
